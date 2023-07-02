347f3a2fc02dc93f8a57408763f1de60e5263ac2
Bastianimatteo <110730345+Bastianimatteo@users.noreply.github.com>
2023-07-02 21:39:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var count_neg = [0,0,0,0,0,0,0,0,0,0,0,0]
 var count_pos = [0,0,0,0,0,0,0,0,0,0,0,0]
 var changes = [0,0,0,0,0,0,0,0,0,0,0,0]
 
+var response, timestamp, open, close = []
+
 const tb = document.getElementById("tbody")
 const tb_change = document.getElementById("tbody_change")
 const n_months = document.getElementById("n_months")
@@ -31,12 +33,12 @@ document.getElementById("form").addEventListener("submit", function(event) {
     {
         if(xhttp.readyState == 4 && xhttp.status == 200)
         {
-            var response = JSON.parse(xhttp.response)
+            response = JSON.parse(xhttp.response)
             if(response.chart.result[0].indicators.quote[0].open)
             {
-                const timestamp = response.chart.result[0].timestamp
-                const open = response.chart.result[0].indicators.quote[0].open
-                const close = response.chart.result[0].indicators.quote[0].close
+                timestamp = response.chart.result[0].timestamp
+                open = response.chart.result[0].indicators.quote[0].open
+                close = response.chart.result[0].indicators.quote[0].close
 
                 tb.innerHTML = "";
                 count = [0,0,0,0,0,0,0,0,0,0,0,0]
@@ -58,44 +60,13 @@ document.getElementById("form").addEventListener("submit", function(event) {
 
                         const date = getNomeMese(month) + " " + year
 
-                        var row = tb.insertRow();
-
-                        var cell_date = row.insertCell();
-                        cell_date.innerHTML = date;
-
-                        var cell_open = row.insertCell();
-                        cell_open.innerHTML = open[i].toFixed(2)
-
-                        var cell_close = row.insertCell();
-                        cell_close.innerHTML = close[i].toFixed(2)
-
-                        var cell_change = row.insertCell();
-                        cell_change.innerHTML = ((close[i] - open[i]) / open[i] * 100).toFixed(2) + " %"
+                        fill_table(date, open[i].toFixed(2), close[i].toFixed(2), ((close[i] - open[i]) / open[i] * 100).toFixed(2) + " %")
                     }
                 }
 
-                tb_change.innerHTML = "";
+                fill_table_change()
 
-                for(var i = 0; i<changes.length; i++)
-                {
-                    var row = tb_change.insertRow();
-
-                    var cell_month = row.insertCell();
-                    cell_month.innerHTML = getNomeMese(i)
-
-                    var cell_change_month = row.insertCell();
-                    cell_change_month.innerHTML = (changes[i] / count[i]).toFixed(2) + " %"
-
-                    var cell_neg = row.insertCell();
-                    cell_neg.innerHTML = count_neg[i]
-
-                    var cell_pos = row.insertCell();
-                    cell_pos.innerHTML = count_pos[i]
-
-                    var cell_percent = row.insertCell();
-                    cell_percent .innerHTML = (count_pos[i]/(count_neg[i] + count_pos[i])*100).toFixed(0) + " %"
-                }
-
+                select_list()
                 colors()
 
                 spinner.style.visibility = "hidden"
@@ -115,6 +86,37 @@ document.getElementById("form").addEventListener("submit", function(event) {
         }
     }
 })
+
+function select_change()
+{
+    const sel = document.getElementById("select").value
+    tb.innerHTML = ""
+
+    for(let i = 0; i < timestamp.length; i++)
+    {
+        if(open[i] && close[i])
+        {
+            const timestamp_date = new Date(timestamp[i] * 1000)
+            const month = (timestamp_date.getMonth()).toString()
+            const year = timestamp_date.getFullYear().toString()
+            const date = getNomeMese(month) + " " + year
+
+            if(sel == -1)
+            {
+                fill_table(date, open[i].toFixed(2), close[i].toFixed(2), ((close[i] - open[i]) / open[i] * 100).toFixed(2) + " %")
+            }
+            else
+            {
+                if(month == sel)
+                {   
+                    fill_table(date, open[i].toFixed(2), close[i].toFixed(2), ((close[i] - open[i]) / open[i] * 100).toFixed(2) + " %")
+                }
+            }
+        }
+    }
+    
+}
+
 
 function save_month(month, change)
 {
@@ -168,4 +170,72 @@ function colors(){
 
     var now = new Date().getMonth();
     tr[now].style.backgroundColor = "lightyellow"
+}
+
+function select_list()
+{
+    const select = document.getElementById("select")
+    var options = [];
+
+    for(let i = 0; i<count.length; i++)
+    {
+        if(count[i] > 0)
+        {
+            options.push(i)
+        }
+    }
+
+    select.innerHTML = "<option value='-1'>All</option>"
+
+    for(let i = 0; i<options.length; i++)
+    {
+        var opt = options[i]
+        var el = document.createElement("option")
+        el.textContent = getNomeMese(opt)
+        el.value = opt
+        select.appendChild(el)
+    }
+}
+
+function fill_table_change()
+{
+    tb_change.innerHTML = "";
+    for(var i = 0; i<changes.length; i++)
+    {
+        var row = tb_change.insertRow();
+
+        var cell_month = row.insertCell();
+        var cell_change_month = row.insertCell();
+        var cell_neg = row.insertCell();
+        var cell_pos = row.insertCell();
+        var cell_percent = row.insertCell();
+
+        cell_month.innerHTML = getNomeMese(i)
+
+        cell_change_month.innerHTML = (changes[i] / count[i]).toFixed(2) + " %"
+
+        cell_neg.innerHTML = count_neg[i]
+
+        cell_pos.innerHTML = count_pos[i]
+
+        cell_percent.innerHTML = (count_pos[i]/(count_neg[i] + count_pos[i])*100).toFixed(0) + " %"
+    }
+}
+
+function fill_table(date, open, close, change)
+{
+    var row = tb.insertRow()
+
+    var cell_date = row.insertCell()
+    var cell_open = row.insertCell()
+    var cell_close = row.insertCell()
+    var cell_change = row.insertCell()
+
+    cell_date.innerHTML = date
+
+    cell_open.innerHTML = open
+
+    cell_close.innerHTML = close
+
+    cell_change.innerHTML = change
 }
